e2f02e176ec1c9b6cef24792d4ab17bae7063b4d
Lavesh Salaskar <73989206+Lavesh03@users.noreply.github.com>
2020-11-23 11:28:35 +0530

    Create Program10.java

---
+++ Program10.java
@@ -0,0 +1,32 @@
+//Program No. 10: Accept marks from the user, if marks greater than 40, declare
+//the student as pass or fail
+import java.util.Scanner;
+
+public class marks
+{
+    public static void main(String args[])
+    {
+        int marks[] = new int[6];
+        int i;
+        float total=0, avg;
+        Scanner scanner = new Scanner(System.in);
+		
+        
+        for(i=0; i<6; i++) { 
+           System.out.print("Enter Marks of Subject"+(i+1)+":");
+           marks[i] = scanner.nextInt();
+           total = total + marks[i];
+        }
+        scanner.close();
+        avg = total/6;
+        System.out.print("The Student's Grade is: ");
+        if(avg>40)
+        {
+            System.out.print("Pass");
+        }
+        else
+        {
+           System.out.print("Fail");
+        } 
+    }
+}

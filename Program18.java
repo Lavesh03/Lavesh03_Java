77fdbb2d13d487b784148882f64c8332eb462dae
Lavesh Salaskar <73989206+Lavesh03@users.noreply.github.com>
2020-12-10 18:56:52 +0530

    Create Program18.java

---
+++ Program18.java
@@ -0,0 +1,21 @@
+interface AnimalEat {
+   void eat();
+}
+interface AnimalTravel {
+   void travel();
+}
+class Animal implementa AnimalEat, AnimalTravel {
+   public void eat() {
+       System.out.printIn("Animal is eating"):
+ }
+   public void travel() {
+       System.out.printIn("Animal is travelling"):
+ }
+}
+public class Demo {
+   public static void main(String args[]) {
+      Animal a = new Animal();
+      a.eat();
+      a.travel();
+ }
+}
